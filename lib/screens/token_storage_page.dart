0ad64b254ca70aaeb1ac79ac90e37db381b73078
igorbasko01 <igorbasko@gmail.com>
2023-07-27 16:26:21 +0300

    return empty page if no tokens are found

--- lib/screens/token_storage_page.dart
+++ lib/screens/token_storage_page.dart
@@ -8,11 +8,43 @@ class TokenStoragePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Token Storage')),
-      body: const Center(
-        child: Text('Token Storage Page'),
-      ),
+    return FutureBuilder<Map<String, String>>(
+      future: tokenStorage.getAll(),
+      builder: (BuildContext context, AsyncSnapshot<Map<String, String>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasError) {
+          return Scaffold(body: Center(child: Text('Error: ${snapshot.error}')));
+        } else {
+          // We have data !
+          Map<String, String> tokens = snapshot.data!;
+          if (tokens.isEmpty) {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Tokens Page'),),
+              body: const Center(child: Text('No tokens found')),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  // Add token code here.
+                },
+                tooltip: 'Add token',
+                child: const Icon(Icons.add),
+              ),
+            );
+          } else {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Tokens Page')),
+              body: const Center(child: Text('Tokens found')),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  // Add token code here.
+                },
+                tooltip: 'Add token',
+                child: const Icon(Icons.add),
+              ),
+            );
+          }
+        }
+      }
     );
   }
 }
