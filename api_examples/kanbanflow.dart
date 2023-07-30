5c2bfdc0c710fe3cb359991c194ed949450326db
igorbasko01 <igorbasko@gmail.com>
2023-07-30 18:43:20 +0300

    add example http requests for kanbanflow (#22)

---
+++ api_examples/kanbanflow.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
+
+void main() {
+  test('kanbanflow get board', () async {
+    var token = Platform.environment['KANBANFLOW_TOKEN'];
+    var response = await http.get(Uri.parse('https://kanbanflow.com/api/v1/board?apiToken=$token'));
+    print(response.body);
+    expect(response.statusCode, 200);
+  });
+
+  test('kanbanflow get tasks of column In Progress in swimlane Personal', () async {
+    var token = Platform.environment['KANBANFLOW_TOKEN'];
+    var response = await http.get(Uri.parse('https://kanbanflow.com/api/v1/tasks?apiToken=$token&columnId=aezrgvwycNgp&swimlaneId=ahgNqFvK36HS'));
+    print(response.body);
+    expect(response.statusCode, 200);
+  });
+}
